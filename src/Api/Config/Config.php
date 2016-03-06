81733f24dc2965b885de14792bc276731a84ebb2
Claudio Zizza <claudio@budgegeria.de>
2016-03-06 20:54:21 +0100

    Change phpdoc return value to  for IDEs

--- src/Api/Config/Config.php
+++ src/Api/Config/Config.php
@@ -304,7 +304,7 @@ abstract class Config
      * @param object|callback $handler The command handler or the callable
      *                                 creating a new command handler on demand.
      *
-     * @return static The current instance.
+     * @return $this The current instance.
      *
      * @see getHandler()
      */
